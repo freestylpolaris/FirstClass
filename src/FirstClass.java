214570a18bf9986955ae372791b3b79a6d966504
it1680505 <it1680505@ou134-206.ad.ilstu.edu>
2013-01-31 14:53:22 -0600

    First commit

---
+++ src/FirstClass.java
@@ -0,0 +1,38 @@
+/*
+ * Modified on <1/22/2013>
+ * 
+ * ULID: <Rkhickm>
+ * Course: IT 168
+ * Lecture section: <4>
+ * Instructor: <Professor Matsuda>
+ * Lab section: <5>
+ * Instructor <Professor Erickson>
+ */
+
+/**
+ * Demonstrates that I performed the pre-lab assignment correctly 
+ * and demonstrates my knowledge of how to re-order and fix errors 
+ * in a pre-written program
+ * 
+ * @author <Ryan Hickman>
+ *
+ */
+
+// My class; which is public
+public class FirstClass {
+	
+	// My main method
+	public static void main (String[] args){
+	
+	// Prints out the statement "Welcome to Java Programming!" onto the user's screen
+	System.out.println("Welcome to Java Programming!");
+	// Prints out the statement "My name is "nnnnn" onto the user's screen; then proceeds to create a new line for the
+	// next statement
+	System.out.print("My name is nnnnnnn.\n");
+	// Prints out the statement "Success" onto the user's screen; then proceeds to create a new line
+	System.out.print("Success!\n");
+	// Creates a new line first then prints the statement "Success!" onto the user's screen
+	System.out.print("\nSuccess!");
+
+	}
+}
